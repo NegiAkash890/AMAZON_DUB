d898afbceec12858c1959a53383c68f7fb639719
Akash Negi <akash8900@gmail.com>
2021-01-10 14:00:40 +0530

    Completed Home Page :)

--- src/Product.js
+++ src/Product.js
@@ -4,16 +4,20 @@ function Product({ title, image, price, Rating }) {
   return (
     <div className="product">
       <div className="product_info">
-        <p>The Alchemist</p>
+        <p>{title}</p>
         <p className="product_price">
           <small>Rs.</small>
-          <strong>200</strong>
+          <strong>{price}</strong>
         </p>
         <div className="product_rating">
-          <p>⭐</p>
+          {Array(Rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
-      <img src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg" />
+      <img src={image} />
       <button>Add to Basket</button>
     </div>
   );
