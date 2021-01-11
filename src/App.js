0766277213f52770f1a11d9c99277c7c640d3f48
Akash Negi <akash8900@gmail.com>
2021-01-12 00:05:14 +0530

    Added Login Route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Login from "./Login";
 import Checkout from "./Checkout";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -8,16 +9,17 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
-          <Router path="/login">
+          <Route path="/login">
             <Login />
-          </Router>
+          </Route>
           <Route path="/checkout">
+            <Header />
             <Checkout />
           </Route>
           {/* Make sure the Home route at bottom*/}
           <Route path="/">
+            <Header />
             <Home />
           </Route>
         </Switch>
